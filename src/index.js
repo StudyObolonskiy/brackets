8a75a433ebac14364e0fe57562ee156359554d21
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-02-19 15:55:06 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let  pattern = bracketsConfig.map((e) => e.join(''))
+    let changedStr = str;
+    for (let i = 0; i < pattern.length; i++) {
+      if (changedStr.includes(pattern[i])) {
+        changedStr = changedStr.replace(pattern[i], '');
+        i = -1;
+      }
+    }
+    if (changedStr.length == 0) {
+        return true;
+    } return false;
 }
